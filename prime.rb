63235c9a1a97f1115a48fa10d432784847b35f61
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-27 01:58:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,29 @@
 # Add  code here!
 
+# def prime?(num)
+#   array = (1..num).to_a
+#   result = array.select {|el| num % el == 0}
+#   if num < 4 && num > 1
+#     true
+#   elsif result.size > 2 || num <= 1
+#     false
+#   else
+#     true
+#   end
+# end
+
 def prime?(num)
-  array = (1..num).to_a
-  result = array.select {|el| num % el == 0}
   if num < 4 && num > 1
     true
-  elsif result.size > 2 || num <= 1
+  elsif num <=1
     false
   else
-    true
+    array = (1..num).to_a
+    result = array.select {|el| num % el == 0}
+    if result.size > 2
+      false
+    else
+      true
+    end
   end
 end
