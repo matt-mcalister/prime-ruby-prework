284657c622928d9f5a9832ee8418ea3cb2b770aa
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-27 02:03:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+
+
+def prime?(num)
+  if num < 4 && num > 1
+    true
+  elsif num <=1 || num == 4
+    false
+  else
+    array = (1..num/2).to_a
+    result = array.select {|el| num % el == 0}
+    if result.size > 2
+      false
+    else
+      true
+    end
+  end
+end
